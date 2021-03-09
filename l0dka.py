00e4fd6c0ee7b641567d64ef3afce84bad431cff
iFla3meZi <71831011+iFla3meZi@users.noreply.github.com>
2021-03-09 21:28:34 +0300

    Add files via upload

---
+++ l0dka.py
@@ -0,0 +1,160 @@
+import asyncio
+import re
+import time
+from time import sleep
+from userbot import CMD_HELP, ZALG_LIST
+from userbot.events import register
+
+@register(outgoing=True, pattern='^.pl(?: |$)(.*)')
+async def typewriter(typew):
+	message = typew.pattern_match.group(1)
+	await typew.edit("`Ща как поплывём...`")
+	sleep(1)
+	await typew.edit("`Плывём: `0%")
+	number = 1
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🌊")
+	number = number+ 1
+	sleep(0.03)
+	await typew.edit(("`Плывём: `") +str(number) + "%   🚣‍♀")
+	number = number+ 1
+	await typew.edit("`Приплыли!`")
